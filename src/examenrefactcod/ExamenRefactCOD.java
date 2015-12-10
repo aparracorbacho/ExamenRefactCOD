d7963da125db032064684e29cc2dff7240838b4e
aparracorbacho <aparracorbacho@atanasoff18.danielcastelao.org>
2015-12-10 09:13:28 +0100

    Creamos metodos

--- src/examenrefactcod/ExamenRefactCOD.java
+++ src/examenrefactcod/ExamenRefactCOD.java
@@ -5,20 +5,15 @@ public class ExamenRefactCOD {
     public static boolean valorCorrecto = false;
 
     public static void main(String arg[]) {
+        Metodos metodos = new Metodos();
         int numDigitos = 3;
         int numeroDigitosActual = 0;
-        if (numDigitos <= 0) {
-            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        }
-        for (int i = 1; i <= 99999; i++) {
+        metodos.digitos0(numDigitos);
+                for (int i = 1; i <= 99999; i++) {
             int auxiliar = i;
-
             int contador = 0;
 
-            while (auxiliar != 0) {
-                auxiliar = auxiliar / 10;
-                contador++;
-            }
+            contador = metodos.bucle1(auxiliar, contador);
             numeroDigitosActual = contador;
 
             if (numeroDigitosActual == numDigitos) {
@@ -31,28 +26,13 @@ public class ExamenRefactCOD {
                         int contador1 = 0;
                         int i1 = 1;
                         int k = (i - 1) / 2;
-                        if (k % 2 == 0) {
-                            k--;
-                        }
-
-                        while (i1 <= k) {
-                            if (i % i1 == 0) {
-                                contador1++;
-                            }
-                            i1 += 2;
-                            if (contador1 == 2) {
-                                i1 = k + 1;
-                            }
-                        }
-
-                        if (contador1 == 1) {
-                            valorCorrecto = true;
-                        }
-                    }
+                        metodos.par(k);
+                        metodos.bucle2(i1,k,contador1,i);
+                    }    
                 }
 
                 if (valorCorrecto == true) {
-                    System.out.println(i);
+                    metodos.imprimirNUM(i);
                 }
             }
         }
